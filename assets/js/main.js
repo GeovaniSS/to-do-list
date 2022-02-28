e04eac9c8ce02be03ea54a4da396456427d2700f
GeovaniSS <geosilvasousa@gmail.com>
2022-02-28 19:55:56 -0300

    [feat]: add task, confirm task e delete task created

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,39 +1,85 @@
-const inputTarefa = document.querySelector('#input-tarefa')
-const botãoAdicionarTarefa = document.querySelector('#adicionar-tarefa')
-const listaDeTarefas = document.querySelector('#lista-de-tarefas')
-let tarefas = []
+const inputTask = document.querySelector('.new-task-input')
+const addTaskButton = document.querySelector('.new-task-button')
+const tasksContainer = document.querySelector('.tasks-container')
 
-botãoAdicionarTarefa.addEventListener('click', () => {
-    if (inputTarefa.value === '') {
-        window.alert('Adicione um tarefa')
+const handleAddNewTask = () => {
+    if(!validInputTask()){
+        inputTask.classList.add('error')
+        return
     }
-    else {   
-        const tarefa = criarTarefa(inputTarefa) // {tag: 'li'}
-        tarefas.push(tarefa)
-        listaDeTarefas.appendChild(tarefa.nome)
-    }
-})
 
-function criarTarefa(inputTarefa) {
-    const li = document.createElement('li')
-    const button = document.createElement('button')
-    button.classList.add('remove-tarefa')
-    li.innerText = `${inputTarefa.value}`
-    button.innerText = 'Remover'
-    li.appendChild(button)
-    return {nome: li, remove: button} 
+    /* Criação da Div que engloba os items*/
+    const taskContainer = document.createElement('div')
+    taskContainer.classList.add('task-container')
+
+    /* Criação do Parágrafo*/
+    const taskContent = document.createElement('p')
+    taskContent.innerText = inputTask.value
+
+    /* Criação do Ícone da Lixeira */
+    const taskDeleteIcon = document.createElement('span')
+    taskDeleteIcon.classList.add('material-icons')
+    taskDeleteIcon.innerText = 'delete'
+
+    taskContainer.appendChild(taskContent)
+    taskContainer.appendChild(taskDeleteIcon)
+    tasksContainer.appendChild(taskContainer)
+
+    /*Limpando Input ao adicionar uma tarefa*/
+    inputTask.value = ''
+    inputTask.focus()
+
+    //const tasks = document.querySelectorAll('.task-container')
+    const tasksContent = document.querySelectorAll('.task-container p')
+    const tasksDeleteIcons = document.querySelectorAll('.task-container span')
+    confirmTasks(tasksContent)
+    removeTasks(tasksDeleteIcons)
 }
 
-listaDeTarefas.addEventListener('click', (e) => {
-    const el = e.target
+const validInputTask = () => {
+    return inputTask.value.trim().length !== 0
+}
 
-    if (el.classList.contains('remove-tarefa')) {
-        for (let i in tarefas) {
-            const {nome, remove} = tarefas[i]
-            remove.addEventListener('click', () => {
-                listaDeTarefas.removeChild(nome)
-            })
+const inputTaskChange = () => {
+    inputTask.classList.remove('error')
+    inputTask.value = ''
+}
+
+const confirmTasks = (tasksContent) => { 
+    for (let taskContent of tasksContent) {
+        taskContent.onclick = () => {
+            let confirmTask = taskContent.classList.contains('confirm-task')
+       
+            if (!confirmTask) {
+                taskContent.classList.add('confirm-task')
+                return
+            }
+    
+            taskContent.classList.remove('confirm-task')
         }
     }
-})
+}
 
+const removeTasks = (tasksDeleteIcons) => {
+    tasksDeleteIcons.forEach((el) => {
+        el.addEventListener('click', () => {
+            el.parentNode.remove() //Remove o Elemento Pai (task-container)
+        })
+    })
+
+   /*for (let taskDeleteIcon of tasksDeleteIcons) {
+        taskDeleteIcon.addEventListener('click', (e) => {
+            taskDeleteIcon.parentNode.remove()
+        })
+   }*/
+}
+
+
+addTaskButton.addEventListener('click', handleAddNewTask)
+inputTask.addEventListener('focus', inputTaskChange)
+inputTask.addEventListener('keydown', (e) => {
+    if (e.keyCode !== 13) {
+        return
+    }
+    handleAddNewTask()
+})
--- readme.mk
+++ readme.mk
@@ -4,19 +4,33 @@
 	- 3° Apagar tarefas - (OK)
 	- 4° ***Salvar tarefas com localStorage***
  
-###Validar Input###
-	- Se o input da tarefa estiver vazio, mostrar um alerta para o usuário 
+###Validar Input### (OK)
+	- Se o input estiver vazio, adicionar uma classe error
+	- Error (Borda vermelha e Placeholder Vermelho)
+	- Se o input deixar de ser vazio, remover a classe error com o evento de change()
 
-###Criar Tarefas###
-	- Criar um elemento pelo JS ('p') com o que foi digitado pelo usuário no input 
-	- Adicionar o elemento a um array vazio []
+###Criar Tarefas### (OK)
+	- Criar os elementos pelo JavaScript 
+	- Div, p e o ícone 
 	- Evento de clicar no botão adicionar
-	- Evento de clicar no enter 
+	- Evento de clicar no enter ***
 
 ###Apagar Tarefas###
-	- Evento de clicar no ícone da lixeira
-	- Remover o elemento do array 
-	
+	- Dentro da função que os items são criados, criar uma variável que vai armazenar a NodeList das divs e a NodeList dos botões
+	- Criar um forEach para percorrer esses elementos e adicionar o evento de click nos botões 
+
+### ATENÇÃO ###
+	- Eu estava errando na manipulação do DOM
+	- Tome cuidado com os eventos
+	- Não se preocupe se o código está grande
+	- Crie da maneira mais detalhada possível 
+	- Não copie o código
+
+## APRENDIZADOS ##
+	- Método trim() nas strings
+	- O addEventListener pode trazer problemas
+	- parentNode / parentNode.remove()
+
 <div class="task-control">
 	<p>Ir na central de transporte do BRB</p>
 	<div class="task-controls">
